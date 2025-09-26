8e4f10c8bdcd6c41bf672d0f9e2ed38490843928
cmoll02 <arthurkubitz@gmail.com>
2025-09-26 11:05:33 +0200

    Überarbeitung Grundstruktur

--- Idee.py
+++ Idee.py
@@ -1,2 +1,2 @@
-'Das ist ein Text mit Spass'ff
-"Ich würde vllt hier noch eine Änderung vornehmen"
+#Die zugrundeliegende Idee ist das Erstellen eines Wortratespiels.
+#Dabei soll man 5 Versuche erhalten, um ein deutschesWort bestehend aus 5 Buchstaben zu erraten.
