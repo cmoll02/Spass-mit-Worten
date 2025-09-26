5820ec8771a591ff8b63461434dcb19bb7996074
cmoll02 <arthurkubitz@gmail.com>
2025-09-26 15:38:04 +0200

    ergänzungen

--- Ausprobieren.py
+++ Ausprobieren.py
@@ -7,13 +7,16 @@ pygame.display.set_caption("Spass mit Worten")
 clock = pygame.time.Clock()
 text_font = pygame.font.Font(None,30)
 
-gut = pygame.Surface((800,50))
+gut = pygame.Surface((800,300))
 gut.fill("blue")
-böse = pygame.Surface((800,50))
+böse = pygame.Surface((800,100))
 böse.fill("red")
 enemy = pygame.Surface((50,50))
 enemy.fill("green")
-enemy_x_pos = 700
+enemy_x_pos = 770
+iche_surface = pygame.Surface((50,50))
+iche_surface.fill("violet")
+iche_rect = iche_surface.get_rect(midleft=(50,250))
 #pygame.image.load(Pfadhiereinfügen) um Bilder zu importieren...erstellt neuen surface
 
 
@@ -23,8 +26,13 @@ while True:
             pygame.quit()
             exit()
     screen.blit(gut,(0,0))
-    screen.blit(böse,(0,100))
+    screen.blit(böse,(0,300))
+    screen.blit(iche_surface,iche_rect)
+    print(iche_rect.left)
     enemy_x_pos -= 5
-    screen.blit(enemy,(enemy_x_pos, 50))
+    screen.blit(enemy,(enemy_x_pos, 250))
+    if enemy_x_pos <= -50:
+        enemy_x_pos = 850
+        enemy.fill("orange")
     pygame.display.update()
     clock.tick(60)
