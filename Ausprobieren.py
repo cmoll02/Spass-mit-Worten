8cc2ed64bf199297c5cccc468ac6ac105feae006
cmoll02 <arthurkubitz@gmail.com>
2025-09-27 08:22:00 +0200

    ergänzungen

--- Ausprobieren.py
+++ Ausprobieren.py
@@ -5,7 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Spass mit Worten")
 clock = pygame.time.Clock()
+
 text_font = pygame.font.Font(None,30)
+score_surf = text_font.render("Score",False, "Black")
+score_rect = score_surf.get_rect(center=(400,50))
+
 
 gut = pygame.Surface((800,300))
 gut.fill("blue")
@@ -13,7 +17,7 @@ böse = pygame.Surface((800,100))
 böse.fill("red")
 enemy = pygame.Surface((50,50))
 enemy.fill("green")
-enemy_x_pos = 770
+enemy_rect = enemy.get_rect(bottomleft=(750,300))
 iche_surface = pygame.Surface((50,50))
 iche_surface.fill("violet")
 iche_rect = iche_surface.get_rect(midleft=(50,250))
@@ -27,12 +31,12 @@ while True:
             exit()
     screen.blit(gut,(0,0))
     screen.blit(böse,(0,300))
+    screen.blit(score_surf,score_rect)
+    screen.blit(enemy,enemy_rect)
+    #print(iche_rect.left)
+    enemy_rect.left -= 5
     screen.blit(iche_surface,iche_rect)
-    print(iche_rect.left)
-    enemy_x_pos -= 5
-    screen.blit(enemy,(enemy_x_pos, 250))
-    if enemy_x_pos <= -50:
-        enemy_x_pos = 850
-        enemy.fill("orange")
+    if enemy_rect.right <= 0:
+        enemy_rect.left = 800
     pygame.display.update()
     clock.tick(60)
