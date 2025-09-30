583383ed769ea4d6de7f86cac2d241556b7a0f10
Juli853 <juliana.schwarz@uni-hamburg.de>
2025-09-30 16:41:25 +0200

    andere bessere Wörterliste
    Signed-off-by: Juli853 <juliana.schwarz@uni-hamburg.de>

--- Wordle.py
+++ Wordle.py
@@ -38,7 +38,7 @@ for Reihe in range(Reihen):
     Tabelle.append(Tabelle_Reihe)
 
 #Wörter laden
-with open("wortliste.txt", "r", encoding="utf-8") as f:
+with open("wordlist-german.txt", "r", encoding="utf-8") as f:
     alle_woerter = [zeile.strip() for zeile in f]
 fuenfer_woerter = [
     wort for wort in alle_woerter
